7368af124fbd61986509b6af678d09158cf35d0d
Koichiro Oishi <bs@v7.com>
2015-06-27 20:16:03 +0900

    Initial Commit

---
+++ testRoundLayerShadow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRoundLayerShadow
+//
+//  Created by bs on 2015/06/27.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testRoundLayerShadow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testRoundLayerShadow
+//
+//  Created by bs on 2015/06/27.
+//  Copyright (c) 2015年 bs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
